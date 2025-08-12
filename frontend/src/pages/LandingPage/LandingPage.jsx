9f31c0716cbe5108172463923fa012533ae74adc
Kayode <drkay4sure@gmail.com>
2025-08-12 14:58:56 +0100

    Feat: Header

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from './Components/Header'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div className='min-h-screen'>
+        <Header />
+    </div>
   )
 }
 
